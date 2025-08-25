ac0e53801945226c4339588a8de680b6108fab80
Rakshitha Hugar A <rakshithahugar@gmail.com>
2025-08-25 14:19:56 +0530

    changes committed Again

--- src/com/demojenkins/Sample.java
+++ src/com/demojenkins/Sample.java
@@ -2,6 +2,6 @@ package com.demojenkins;
 
 public class Sample {
 	public static void main(String [] args) {
-		System.out.println("Hello jenkin git.");
+		System.out.println("Hello Jenkins Git Integration! - Updated\"");
 	}
 }
